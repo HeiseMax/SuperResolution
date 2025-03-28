18fb0014239e22ec8d825ec7b8eb44ada12f37d8
mace-ksl <kessler.marcel2000@gmail.com>
2025-03-28 13:59:27 +0100

    LPIPS models

--- utils/utils.py
+++ utils/utils.py
@@ -33,18 +33,33 @@ def get_3x3(in_dim, out_dim):
 class PerceptualLoss(nn.Module):
     def __init__(self):
         super().__init__()
-        vgg = models.vgg16(pretrained=True).features[:16]  # Use first layers
+        vgg = models.vgg16(pretrained=True).features[:16]  # conv1_1 to relu4_1
+        #vgg = models.vgg16(pretrained=True).features[:4]  # conv1_1 + relu1_1
+
         for param in vgg.parameters():
-            param.requires_grad = False  # Freeze VGG weights
+            param.requires_grad = False
         self.vgg = vgg
+
+        self.register_buffer('mean', torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
+        self.register_buffer('std', torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+
+    def normalize(self, x):
+        # Assume x in [0, 1]
+        return (x - self.mean) / self.std
 
     def forward(self, pred, target):
         if pred.shape[1] == 1:
             pred = pred.repeat(1, 3, 1, 1)
             target = target.repeat(1, 3, 1, 1)
+
+        pred = self.normalize(pred.clamp(0, 1))
+        target = self.normalize(target.clamp(0, 1))
+
         pred_features = self.vgg(pred)
         target_features = self.vgg(target)
-        return F.mse_loss(pred_features, target_features)  # Feature-level loss
+
+        return F.mse_loss(pred_features, target_features)
+
 
 
 def save_model(model, path):
